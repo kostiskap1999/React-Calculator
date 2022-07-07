212f22f5ed2fcef0498ce017f00f78e9a153d407
kostiskap1999 <kostiskap1999@hotmail.com>
2022-07-07 13:05:58 +0300

    Calculations can start with negative number

--- src/Functions/Calculator.js
+++ src/Functions/Calculator.js
@@ -45,44 +45,51 @@ export function calculate(expr) {
     if(numbers == null)
         return 0;
 
-    if (operations == null)
-        return parseFloat(expr)
-        
-    if (numbers.length === 0 || expr.match(regexWhole)[0].length !== expr.length)
+    if(expr[0] == ADD || expr[0] == SUB)
+        numbers.unshift(0)
+    else if(expr.match(regexWhole)[0].length !== expr.length)
         return "Wrong Expression";
+    
+    if (operations == null)
+        result = expr;
+    else{
+        if (numbers.length === operations.length)
+            if (operations[operations.length-1] === ADD || operations[operations.length-1] === SUB)
+                numbers[numbers.length] = 0;
+            else
+                numbers[numbers.length] = 1;
 
-    if (numbers.length === operations.length){
-        console.log(numbers)
-        if (operations[operations.length-1] === ADD || operations[operations.length-1] === SUB)
-            numbers[numbers.length] = 0;
-        else
-            numbers[numbers.length] = 1;
-        console.log(numbers)
-    }
-
-    var i;
-    //while there are operations
-    while(operations.length !== 0){
-        if (operations.includes(POW) | operations.includes(POW_ALT)){
-            i = operations.indexOf(POW)
-            result = eval(numbers[i] + POW_ALT + numbers[i+1]);
-        }else if (operations.includes(DIV)){
-            i = operations.indexOf(DIV)
-            result = eval(numbers[i] + DIV + numbers[i+1]);
-        }else if (operations.includes(MUL)){
-            i = operations.indexOf(MUL)
-            result = eval(numbers[i] + MUL + numbers[i+1]);
-        }else if (operations.includes(SUB)){
-            i = operations.indexOf(SUB)
-            result = eval(numbers[i] + SUB + numbers[i+1]);
-        }else if (operations.includes(ADD)){
-            i = operations.indexOf(ADD)
-            result = eval(numbers[i] + ADD + numbers[i+1]);
+        var i;
+        //while there are operations
+        while(operations.length !== 0){
+            if (operations.includes(POW) | operations.includes(POW_ALT)){
+                i = operations.indexOf(POW)
+                result = eval(numbers[i] + POW_ALT + numbers[i+1]);
+            }else if (operations.includes(DIV)){
+                i = operations.indexOf(DIV)
+                result = eval(numbers[i] + DIV + numbers[i+1]);
+            }else if (operations.includes(MUL)){
+                i = operations.indexOf(MUL)
+                result = eval(numbers[i] + MUL + numbers[i+1]);
+            }else if (operations.includes(SUB)){
+                i = operations.indexOf(SUB)
+                result = eval(numbers[i] + SUB + numbers[i+1]);
+            }else if (operations.includes(ADD)){
+                i = operations.indexOf(ADD)
+                result = eval(numbers[i] + ADD + numbers[i+1]);
+            }
+            operations.splice(i, 1);
+            numbers.splice(i, 2, result);
         }
-        operations.splice(i, 1);
-        numbers.splice(i, 2, result);
+
     }
 
-    return parseFloat(result.toFixed(9));
+    
+    var largeNumberRegex = new RegExp(`e+`, 'g')
+
+    if(result.toString().match(largeNumberRegex))
+        return result.toString();
+    else
+        return result.toLocaleString('en-US', { maximumFractionDigits: 9 });
 
 }
